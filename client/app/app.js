c93f167515c686b4db81d2856e5e3b5d171ec316
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-31 16:55:17 -0700

    Updates ViewPort photo on photo title click

--- client/app/app.js
+++ client/app/app.js
@@ -2,4 +2,8 @@ angular.module('albumApp', [])
 .controller('albumController', function($scope) {
   $scope.photos = initialPhotos;
   $scope.currentPhoto = initialPhotos[0];
+
+  $scope.setCurrentPhoto = function(photo) {
+    $scope.currentPhoto = photo;
+  }
 });
