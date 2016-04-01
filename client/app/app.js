e804f06e532093dc3362fbc679d58a397cfd4a29
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-31 17:25:57 -0700

    Renders rating and allows updating via drop-down menu

--- client/app/app.js
+++ client/app/app.js
@@ -2,8 +2,17 @@ angular.module('albumApp', [])
 .controller('albumController', function($scope) {
   $scope.photos = initialPhotos;
   $scope.currentPhoto = initialPhotos[0];
+  $scope.currentRating = $scope.currentPhoto.rating;
+  $scope.ratings = [-1, 0, 1];
 
   $scope.setCurrentPhoto = function(photo) {
     $scope.currentPhoto = photo;
+    $scope.currentRating = $scope.currentPhoto.rating;
   }
+
+  $scope.updateRating = function(rating) {
+    $scope.currentPhoto.rating = rating;
+    $scope.currrentRating = rating;
+  };
+
 });
